d68d06ff4d2489fe8a593c4025ed484334df4735
WildanMZaki <wildanmzaki7@gmail.com>
2023-11-05 11:11:48 +0700

    LikeOrDislikeCommentUseCase Fix

--- src/Applications/use_case/comments/LikeOrDislikeCommentUseCase.js
+++ src/Applications/use_case/comments/LikeOrDislikeCommentUseCase.js
@@ -5,7 +5,20 @@ class LikeOrDislikeCommentUseCase {
     this._threadRepository = threadRepository;
   }
 
-  async execute(useCasePayload) {}
+  async execute(useCasePayload) {
+    const { thread_id, comment_id, user_id } = useCasePayload;
+    await this._threadRepository.verifyIsThreadExists(thread_id);
+    await this._commentRepository.verifyIsCommentExists(comment_id);
+    const userHasLike = await this._commentLikeRepository.hasLike(
+      user_id,
+      comment_id
+    );
+    if (!userHasLike) {
+      await this._commentLikeRepository.like(user_id, comment_id);
+    } else {
+      await this._commentLikeRepository.dislike(user_id, comment_id);
+    }
+  }
 }
 
 module.exports = LikeOrDislikeCommentUseCase;
